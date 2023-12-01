5a78f89d7e191f899db76a899e29e1c2e29ade49
JackSparrow <jack_sparrow@kwunphi.com>
2023-12-01 17:53:52 +0800

    fix:修改开发环境日志级别

--- src/main/java/com/ruoyi/project/official/controller/OfficialController.java
+++ src/main/java/com/ruoyi/project/official/controller/OfficialController.java
@@ -1,6 +1,9 @@
 package com.ruoyi.project.official.controller;
 
 import com.ruoyi.framework.web.controller.BaseController;
+import com.ruoyi.project.official.domain.Course;
+import com.ruoyi.project.official.mapper.CourseDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class OfficialController extends BaseController {
 
 
+    @Autowired
+    private CourseDao courseDao;
 
     @GetMapping("/test")
-    public String test(){
-
-
-        return "你好";
+    public Course test(){
+        Course course = courseDao.selectById(1);
+        return course;
     }
 
 
